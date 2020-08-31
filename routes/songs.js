55598e49f5d9ba0cbc8c554967026622ea7accf3
Miguel Zambrano <miguelz@email.sc.edu>
2020-08-31 16:17:22 -0400

    song routes done

--- routes/songs.js
+++ routes/songs.js
@@ -37,32 +37,100 @@ router.post('/', async (req,res) => {
         title: req.body.title,
         albumTitle: req.body.albumTitle,
         genre: req.body.genre,
-        //albumCover: fileName,
-        artist: req.body.artist,
+        artist: req.body.artist
         
     })
 
     saveCover(song, req.body.cover)
     try{
         const newSong = await song.save()
-        res.redirect('songs')
+        res.redirect(`songs/${newSong.id}`)
     }catch{
         renderNewPage(res, song, true)
     }
 })
 
+//show song route
+router.get('/:id', async (req,res) => {
+    try{
+        const song = await Song.findById(req.params.id).populate('artist').exec()
+        res.render('songs/show', { song: song})
+    }catch{
+        res.redirect('/')
+    }
+})
 
+
+//edit songs route
+router.get('/:id/edit', async (req, res) => {
+    try{
+        const song = await Song.findById(req.params.id)
+        renderEditPage(res, song)
+    }catch{
+        res.redirect('/')
+    }
+})
+
+//update song route
+router.put('/:id', async (req,res) => {
+    let song
+    try{
+        song = await Song.findById(req.params.id)
+        song.title = req.body.title
+        song.albumTitle = req.body.albumTitle
+        song.genre = req.body.genre
+        song.artist = req.body.artist
+        if(req.body.cover != null && req.body.cover != ''){
+            saveCover(song, req.body.cover)
+        }
+        await song.save()
+        res.redirect(`/songs/${song.id}`)
+    }catch{
+        if(song != null){
+        renderEditPage(res, song, true)
+        }else{
+            redirect('/')
+        }
+    }
+})
+
+//delete song route
+router.delete('/:id', async (req,res) => {
+    let song
+    try{
+        song = await Song.findById(req.params.id)
+        await song.remove()
+        res.redirect('/songs')
+    }catch{
+        if(song != null){
+            res.render('songs/show', {
+               song: song,
+               errorMessage: 'Erroring removing song' 
+            })
+        }else{
+            res.redirect('/')
+        }
+    }
+})
 
 
 async function renderNewPage(res, song, hasError = false){
+    renderFormPage(res, song, 'new', hasError)
+}
+
+async function renderEditPage(res, song, hasError = false){
+    renderFormPage(res, song, 'edit', hasError)
+}
+
+async function renderFormPage(res, song, form, hasError = false){
     try{
         const artists = await Artist.find({})
         const params = {
             artists: artists,
             song: song
         }
-        if (hasError) params.errorMessage = 'Error Creating Song'
-        res.render('songs/new', params)
+        if (hasError) params.errorMessage = 'Error Uploading Song'
+        res.render(`songs/${form}`, params)
     }catch{
         res.redirect('/songs')
     }
